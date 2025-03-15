5a12ee2a8a3a335fc5baf18afd53bc04ecca7178
Ian Scrivener <you@example.com>
2025-03-15 16:53:40 +1100

    finalise provisioning script

--- PROVISIONING_SCRIPT.sh
+++ PROVISIONING_SCRIPT.sh
@@ -4,13 +4,15 @@ curl -o /usr/local/bin/pget -L "https://github.com/replicate/pget/releases/lates
 chmod +x /usr/local/bin/pget
 
 
+########################
+# get manifest and run pget - pget is a fast parallel file getter
+cd /workspace/
+wget https://raw.githubusercontent.com/ianscrivener/ai-dock-comfyui/refs/heads/main/pget_manifest.txt
+pget multifile pget_manifest.txt
 
-
-wget 
  
-
-
-# Custom nodes
+########################
+# install custom nodes
 cd /workspace/ComfyUI/custom_nodes 
 git clone https://github.com/cubiq/ComfyUI_IPAdapter_plus
 git clone https://github.com/pythongosssss/ComfyUI-Custom-Scripts
@@ -24,7 +26,6 @@ git clone https://github.com/mav-rik/facerestore_cf
 git clone https://github.com/ltdrdata/ComfyUI-Impact-Subpack
 git clone https://github.com/shingo1228/ComfyUI-SDXL-EmptyLatentImage
 git clone https://github.com/ltdrdata/ComfyUI-Impact-Pack
-
 
 
  
